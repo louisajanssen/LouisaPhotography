642c05b68805de3f943ea376c71e1f34d061743a
louisajanssen <67242249+louisajanssen@users.noreply.github.com>
2020-09-23 11:47:24 -0700

    make images clickable, add icons

--- js/script.js
+++ js/script.js
@@ -22,4 +22,14 @@ function SubmitIt() {
     }
 }
 
+$('#Fullscreen').css('height', $(document).outerWidth() + 'px');
+    
+$('.port').click(function () {
+    var src = $(this).attr('src'); 
+    $('#Fullscreen img').attr('src', src); 
+    $('#Fullscreen').fadeIn();
+});
 
+$('#Fullscreen').click(function () {
+    $(this).fadeOut();  
+});
